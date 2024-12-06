589e273ecda9b6b459d7c906f563e72045f26ead
Gautham Venkataraman <gauthsvenkat@gmail.com>
2024-12-06 16:52:27 +0100

    highlight on yank nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,3 +7,14 @@
 -- - gcc, make, cmake (a bunch of stuff)
 require("config.options")
 require("config.lazy")
+
+-- Highlight on yank
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
